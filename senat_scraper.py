61a768ff38fc9abced793663898e95c80426b1bb
Bart Klimowski <89873291+hrspxmc@users.noreply.github.com>
2023-12-31 03:05:37 +0100

    update

--- senat_scraper.py
+++ senat_scraper.py
@@ -21,7 +21,8 @@ import logging
 import datetime	
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-
+import shelve
+import pandas as pd
 
 #%%
 
@@ -47,6 +48,7 @@ def get_voting_dict(voting_link):
         
     return voting_dict
 
+voting_date_dict = {}
 
 def process_personal_votings(voting_id):
     driver.get(voting_page_template.format(voting_id))
@@ -55,12 +57,17 @@ def process_personal_votings(voting_id):
 
     for row in soup.find('table', {'class': 'voting-table'}).tbody.findAll('tr'):
         voting_links_dict[row.findAll('td')[0].get_text().strip()] = row.findAll('td')[1].find('a')['href']
+        voting_date_dict[row.findAll('td')[0].get_text().strip()] = row.findAll('td')[1].get_text().strip()
         
     votings = {key: get_voting_dict(val) for key,val in voting_links_dict.items()}
 
     return votings
 
 #%%
+
+options = Options()
+options.add_argument('--headless')
+driver = webdriver.Firefox(options=options)
 
 senat_page = 'https://www.senat.gov.pl'
 voting_page_template = 'https://www.senat.gov.pl/sklad/senatorowie/aktywnosc-glosowania,{}.html'
@@ -69,104 +76,48 @@ project_root = "/home/hrspx/Documents/GitHub/scraper_parlament/"
 
 #%%
 
-kadencja = 9
+kadencja = 10
 
-with open("senat_strony/senatorowie_IX.html") as html_page:
+with open("senat_strony/senatorowie_X.html") as html_page:
     soup = BeautifulSoup(html_page, 'html.parser')
     links = [ii.find('a')['href'] for ii in soup.find_all('div', {'class': 'senator-kontener'})]     
     
 #%%
-
 senator_id = [ii.split(',')[1] for ii in links]
-
-
-process_personal_votings(senator_id[1]+ ',' + str(kadencja))
-
-#%%
-
-options = Options()
-options.add_argument('--headless')
-driver = webdriver.Firefox(options=options)
-
 res_dict = {}
 
 for id in tqdm(senator_id, position=0, leave=True):
-    print(id)
     res_dict[id] = process_personal_votings(id + ',' + str(kadencja))
 
-
 #%%
 
+klub = {}
+senator = {}
 
-
-#%%
-
-single_batch_size = 5
-n_jobs = 4
-chunk_size = n_jobs * single_batch_size
-parallel = Parallel(n_jobs=n_jobs)
-
-with shelve.open(project_root + 'data/senat_votes') as db:
-    remaining_keys = [ii for ii in senator_id if repr(ii) not in db.keys()]
-
-#%%
-
-while remaining_keys:
-    chunks = [
-        remaining_keys[x : x + chunk_size]
-        for x in range(0, len(remaining_keys), chunk_size)
-    ]
+for id, ii in tqdm(zip(senator_id, links), position=0, leave=True):
+    time.sleep(1)
+    driver.get(ii)
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    senator[id] = soup.find('div', {'class': 'informacje'}).find('h2').get_text()
+    klub[id] = soup.find('div', {'class': 'kluby'}).find('a').get_text()
     
-    for ii in tqdm(chunks):
-        try:
-            output = parallel(
-                delayed(process_personal_votings)(senator_id + ',' + str(kadencja)) for senator_id in ii
-            )
-
-            with shelve.open(project_root + "data/process_db") as db:
-                for key, value in zip(ii, output):
-                    db[repr(key)] = value
-        except ConnectionError as e:
-            print(e)
-            pass
-
-    with shelve.open(project_root + "data/senat_votes") as db:
-        remaining_keys = [ii for ii in senator_id if repr(ii) not in db.keys()]
-
-
-#%% debug
-
-voting_id = senator_id[1]+ ',' + str(kadencja)
-
-driver.get(voting_page_template.format(voting_id))
-soup = BeautifulSoup(driver.page_source, "html.parser")
-voting_links_dict = {}
-
-for row in soup.find('table', {'class': 'voting-table'}).tbody.findAll('tr'):
-    voting_links_dict[row.findAll('td')[0].get_text().strip()] = row.findAll('td')[1].find('a')['href']
-        
 
 #%%
 
-voting_link = voting_links_dict['22']
+# unpac dict
 
-driver.get(senat_page+voting_link)
-soup = BeautifulSoup(driver.page_source, "html.parser")
+res_list = []
 
-voting_dict = {}
+for key,val in res_dict.items():
+    for ik, iv in val.items():
+        tmp_df = pd.DataFrame.from_dict(iv, orient='index')
+        tmp_df['senator'] = senator[key]
+        tmp_df['nr_posiedzenia'] = ik
+        tmp_df['klub'] = klub[key]
+        tmp_df['kedencja'] = kadencja
+        tmp_df['data'] = voting_date_dict[ik]
+        res_list.append(tmp_df)
 
-#%%
-
-for row in soup.find('table', {'class': 'tabela-aktywnosc-szczegoly-glosowania-dzien'}).tbody.findAll('tr'):
-    vote_dict = {
-        'vote': row.findAll('td')[2].get_text().strip(),
-        'title': row.findAll('td')[3].find(string=True,recursive=False).get_text().strip(),
-        'sub': secure_get_text(row)
-        }
-        
-    voting_dict[row.findAll('td')[0].get_text().strip()] = vote_dict
-        
-
-
-
-
+df = pd.concat(res_list)
+df.index.name = 'nr_glosowania'
+df.to_excel("test_senat.xlsx")
